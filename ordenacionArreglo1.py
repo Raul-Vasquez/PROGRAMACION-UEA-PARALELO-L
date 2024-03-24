d56ca574f41368022732f14618ee8f881e158339
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2024-03-24 17:48:28 -0500

    Diccionario

--- ordenacionArreglo1.py
+++ ordenacionArreglo1.py
@@ -1,12 +1,14 @@
 # Ordenar el arreglo Bidimensional de 3 filas por 3 columnas
 arregloBidimensional = [
-                        [2,8,5],
+                        [9,11,5],
                         [4,0,5],
-                        [3,4,5]
+                        [3,1,5]
                     ]
+
 for fila in range (len(arregloBidimensional)):
     for columna in range (len(arregloBidimensional[fila])):
         if columna == len (arregloBidimensional[fila])-1:
+            varTemporal = 0
             print (arregloBidimensional[fila][columna])
 
         else:
@@ -16,13 +18,15 @@ for fila in range (len(arregloBidimensional)):
                                     #2                                        8
                                # [0]     [1] >                           [0]     [2]
                                    # 8                                        5
+
                 varTemporal = arregloBidimensional [fila][columna]
+
                 arregloBidimensional [fila][columna] =  arregloBidimensional [fila][columna + 1]
-            # if arregloBidimensional [fila][columna+1]:
+                #if arregloBidimensional [fila][columna+1]:
 
                 arregloBidimensional [fila][columna +1] = varTemporal
                 print (arregloBidimensional [fila][columna])
-                print (arregloBidimensional [fila][columna+1])
+                #print (arregloBidimensional [fila][columna+1])
             else:
                 print (arregloBidimensional [fila][columna])
 
