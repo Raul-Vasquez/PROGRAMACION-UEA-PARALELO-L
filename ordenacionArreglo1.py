116df1eda6c6422da130493f6ab4f368db35a838
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2024-03-26 18:52:51 -0500

    Practica de arreglo

--- ordenacionArreglo1.py
+++ ordenacionArreglo1.py
@@ -1,36 +1,36 @@
 # Ordenar el arreglo Bidimensional de 3 filas por 3 columnas
 arregloBidimensional = [
                         [9,11,5],
-                        [4,0,5],
-                        [3,1,5]
+                        [4,1,5],
+                        [9,1,5]
                     ]
 
 for fila in range (len(arregloBidimensional)):
-    for columna in range (len(arregloBidimensional[fila])):
-        if columna == len (arregloBidimensional[fila])-1:
-            varTemporal = 0
-            print (arregloBidimensional[fila][columna])
+    for k in range (len(arregloBidimensional)):
+                for columna in range (len(arregloBidimensional[fila])):
+                    if columna  <  len (arregloBidimensional[fila]) -1:
+                       #print (arregloBidimensional[fila][columna])
 
-        else:
+                    #else:
 
-            if arregloBidimensional [fila][columna] > arregloBidimensional [fila][columna +1]:
-                               # [0]     [0] >                           [0]     [1]
-                                    #2                                        8
-                               # [0]     [1] >                           [0]     [2]
-                                   # 8                                        5
+                        if arregloBidimensional [fila][columna] > arregloBidimensional [fila][columna +1]:
+                                           # [0]     [0] >                           [0]     [1]
+                                                #2                                        8
+                                           # [0]     [1] >                           [0]     [2]
+                                               # 8                                        5
 
-                varTemporal = arregloBidimensional [fila][columna]
+                            varTemporal = arregloBidimensional [fila][columna]
 
-                arregloBidimensional [fila][columna] =  arregloBidimensional [fila][columna + 1]
-                #if arregloBidimensional [fila][columna+1]:
+                            arregloBidimensional [fila][columna] =  arregloBidimensional [fila][columna + 1]
+                            #if arregloBidimensional [fila][columna+1]:
 
-                arregloBidimensional [fila][columna +1] = varTemporal
-                print (arregloBidimensional [fila][columna])
-                #print (arregloBidimensional [fila][columna+1])
-            else:
-                print (arregloBidimensional [fila][columna])
-
-
+                            arregloBidimensional [fila][columna +1] = varTemporal
+                            #                          print (arregloBidimensional [fila][columna])
+                            #print (arregloBidimensional [fila][columna+1])
+                       #else:
+                           #print (arregloBidimensional [fila][columna])
+print (arregloBidimensional)
+# Profe se me va la señal esta mala la conexion y no le esucho bien le escribi a su correo tambien institucional
 
 
 
